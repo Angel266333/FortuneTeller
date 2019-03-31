17be6d19569dffb1ac25ea9086fe5cdb140f5567
Angel Petrov <angelilianov@gmail.com>
2019-03-31 20:32:17 +0200

    Make use of Google Fire base on login

---
+++ app/src/main/java/com/angel266489/fortuneteller/LoginActivity.java
@@ -0,0 +1,94 @@
+package com.angel266489.fortuneteller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    SignInButton signInButton;
+    GoogleApiClient mGoogleApiClient;
+    private final static String KEY = "";
+
+    private static final int RC_SIGN_IN = 2600;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        signInButton = findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString
+                (R.string.default_web_client_id)).requestEmail().build();
+        mAuth = FirebaseAuth.getInstance();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+    }
+
+
+    public void signIn(View view) {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+
+        } else {
+            showToast();
+        }
+    }
+
+    private void showToast() {
+        Toast.makeText(this, "Login failed :(", Toast.LENGTH_LONG).show();
+    }
+
+    public void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            GoogleSignInAccount acc = result.getSignInAccount();
+            Intent intent = new Intent(this, MainActivity.class);
+            if(acc.getEmail() != null) {
+                intent.putExtra(KEY, acc.getEmail());
+            }
+            // TODO: Print toast :)
+            startActivity(intent);
+        }
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        showToast();
+    }
+
+    @Override
+    public void onClick(View v) {
+        signIn(v);
+    }
+}
--- app/src/main/java/com/angel266489/fortuneteller/MainActivity.java
+++ app/src/main/java/com/angel266489/fortuneteller/MainActivity.java
@@ -1,11 +1,15 @@
 package com.angel266489.fortuneteller;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 // Thanks for the tutorials and inspiration for some of the code from the user https://github.com/haroon47.
 // All credits go to their respective owners.
@@ -14,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     DrawerLayout drawerLayout;
+    private static final int RC_SIGN_IN = 2600;
+    private final static String KEY = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,16 @@ public class MainActivity extends AppCompatActivity {
             drawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == RC_SIGN_IN) {
+            String returnValue = data.getExtras().getString(KEY);
+            Toast.makeText(this, returnValue, Toast.LENGTH_LONG).show();
         }
     }
 }
